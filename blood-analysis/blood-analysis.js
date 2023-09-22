07c3d7943a73d004edff2e5db52efe618a9ccd33
Nancy Taneva <nancy.taneva@gmail.com>
2023-09-22 20:12:45 +0300

    blood analysis version 2

--- blood-analysis/blood-analysis.js
+++ blood-analysis/blood-analysis.js
@@ -1,9 +1,26 @@
+
 const panelIndex     = document.querySelectorAll('.panel-index');
 
 panelIndex.forEach(button => {
     
     button.addEventListener('click', () => {
         panelIndex.forEach(b => b.classList.remove('selected'));
+        button.classList.add('selected');
+
+        // const buttonId = button.id;
+        // console.log(`panelIndex: ${chosenPet}`);
+    });
+});
+
+
+
+
+const panel     = document.querySelectorAll('.panel');
+
+panel.forEach(button => {
+    
+    button.addEventListener('click', () => {
+        panel.forEach(b => b.classList.remove('selected'));
         button.classList.add('selected');
 
         // const buttonId = button.id;
